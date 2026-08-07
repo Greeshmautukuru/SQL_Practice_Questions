dcc74fad7cfb832d4bbb57c6d3f63172532aabb7
Greeshmautukuru <greeshmautukuru@gmail.com>
2026-08-07 11:12:49 +0530

    Create 02_DML.sql
    Added DML commands

---
+++ 02_DML.sql
@@ -0,0 +1,17 @@
+/* SQL _DML COMMANDS */
+
+1.INSERT:#data add
+</> SQL :
+INSERT INTO TABLE_NAME (column1_name, column2_name, column3_name, ...)
+VALUES (value1, value2, value3, ...);
+
+2.UPDATE:#data update
+</> SQL :
+UPDATE employees
+SET salary = 50000
+WHERE emp_id = 1;
+
+3.DELETE:#data delete
+</> SQL :
+DELETE FROM employees
+WHERE emp_id = 1;
