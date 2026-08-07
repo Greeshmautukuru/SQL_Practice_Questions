4df86d3a55c225af79a590dfb63c84a94994fbb6
Greeshmautukuru <greeshmautukuru@gmail.com>
2026-08-07 11:26:38 +0530

    Create 03_DQL.sql
    Added DQL commands

---
+++ 03_DQL.sql
@@ -0,0 +1,26 @@
+/* DQL COMMANDS */
+
+1.SELECT:#IT IS USED TO GET DATA FROM TABLES
+</> SQL :
+SELECT column1_name, column2_name, column3_name, ...
+FROM TABLE_NAME;
+
+2.SELECT ALL COLUMNS:#IT IS USED TO GET ALL COLUMNS FROM TABLES
+</> SQL :  
+SELECT * FROM TABLE_NAME;
+
+3.WHERE:# WHERE is used to filter rows based on a condition.
+</>  SQL:
+SELECT *FROM EMPLOYEE
+WHERE salary > 50000;
+
+4.ORDER BY:#ORDER BY is used to sort the result set in ascending or descending order.
+</> SQL:
+SELECT*FROM EMPLOYEE 
+ORDER BY SALARY DESC;
+
+5.LIMIT:#LIMIT is used to specify the maximum number of records to return.
+</> SQL:
+SELECT * FROM EMPLOYEE
+LIMIT 10;
+
